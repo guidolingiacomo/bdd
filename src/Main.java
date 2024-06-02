4373c2eb0f9d697651bd310f3d9310d932382632
guidolingiacomo <giacomo.guidolin@hotmail.com>
2024-06-02 21:26:32 +0200

    first

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,23 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/musica";
         String user = "root";
         String password = "***";
-        int BranoID = 1;
-        int UtenteID = 4;
 
         try {
             Connection mydb = DriverManager.getConnection(url, user, password);
 
-            String sql = "SELECT Battuta.Numero, Battuta.BTM, Battuta.Commento FROM Battuta WHERE Battuta.Brano = " + BranoID + " AND Battuta.Utente = " + UtenteID + ";";
+            String sql = "SELECT * FROM Battuta;";
             PreparedStatement mycursor = mydb.prepareStatement(sql);
 
             ResultSet myresult = mycursor.executeQuery();
 
             while (myresult.next()) {
                 int num = myresult.getInt("Numero");
-                String btm = myresult.getString("BTM");
+                int brano = myresult.getInt("Brano");
+                int btm = myresult.getInt("BTM");
                 String commento = myresult.getString("Commento");
-                String data_lancio = myresult.getString("data_lancio");
+                int utente = myresult.getInt("Utente");
 
-                System.out.println("Numero di battuta: " + num + ", BTM: " + btm + ", Commento: " + commento );
+                System.out.println("Numero di battuta: " + num + ", Brano: " + brano + ", BTM: " + btm + ", Commento: " + commento + ", Utente: " + utente);
             }
         } catch (SQLException e) {
             System.out.println("SQL Exception: " + e.getMessage());
